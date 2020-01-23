2bf1e8abf4717cd562554f9afd2c3f4164d60ea4
theshekhargupta <theshekhargupta@gmail.com>
2020-01-23 14:33:49 +0530

    Disabling iOS SDK

--- react-native-userexperior/UserExperior.js
+++ react-native-userexperior/UserExperior.js
@@ -5,109 +5,142 @@ var UserExperiorBridge = NativeModules.UserExperior;
 class UserExperior {
 
 	static startRecording(ueSdkAppVersionKey) {
-		if (!ueSdkAppVersionKey) {
-            console.log('UserExperior : startRecording params are invalid');
-            return;
+        if (Platform.OS === 'android') {
+            if (!ueSdkAppVersionKey) {
+                console.log('UserExperior : startRecording params are invalid');
+                return;
+            }
+            UserExperiorBridge.startRecording(ueSdkAppVersionKey);    
         }
-        UserExperiorBridge.startRecording(ueSdkAppVersionKey);
     }
 
     static stopRecording() {
-        UserExperiorBridge.stopRecording();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.stopRecording();
+        }
     }
 
 
     static pauseRecording() {
-        UserExperiorBridge.pauseRecording();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.pauseRecording();
+        }
     }
 
     static resumeRecording() {
-        UserExperiorBridge.resumeRecording();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.resumeRecording();
+        }
     }
 	
 	static setUserIdentifier(userIdentifier) {
-        if (!userIdentifier) {
+        if (Platform.OS === 'android') {
+            if (!userIdentifier) {
             console.log('UserExperior : setUserIdentifier params are invalid');
             return;
+            }
+            UserExperiorBridge.setUserIdentifier(userIdentifier);
         }
-        UserExperiorBridge.setUserIdentifier(userIdentifier);
     }
 
     static setCustomTag(customTag, type) {
-        if (!customTag && !type) {
-            console.log('UserExperior : setCustomTag params are invalid');
-            return;
+        if (Platform.OS === 'android') {
+            if (!customTag && !type) {
+                console.log('UserExperior : setCustomTag params are invalid');
+                return;
+            }
+            UserExperiorBridge.setCustomTag(customTag, type);
         }
-        UserExperiorBridge.setCustomTag(customTag, type);
     }
 
 
-    /*static startScreen(screenName) {
-        if (!screenName) {
-            console.log('UserExperior : startScreen params are invalid');
-            return;
-        }
-        UserExperiorBridge.startScreen(screenName);
+    static startScreen(screenName) {
+        if (Platform.OS === 'android') {
+            if (!screenName) {
+                console.log('UserExperior : startScreen params are invalid');
+                return;
+            }
+            UserExperiorBridge.startScreen(screenName);
+    }
     }
 	
 	static startTimer(timerName) {
-        if (!timerName) {
-            console.log('UserExperior : startTimer params are invalid');
-            return;
+        if (Platform.OS === 'android') {
+            if (!timerName) {
+                console.log('UserExperior : startTimer params are invalid');
+                return;
+            }
+            UserExperiorBridge.startTimer(timerName);
         }
-        UserExperiorBridge.startTimer(timerName);
     }
 	
 	static endTimer(timerName) {
-        if (!timerName) {
-            console.log('UserExperior : endTimer params are invalid');
-            return;
+        if (Platform.OS === 'android') {
+            if (!timerName) {
+                console.log('UserExperior : endTimer params are invalid');
+                return;
+            }
+            UserExperiorBridge.endTimer(timerName);
         }
-        UserExperiorBridge.endTimer(timerName);
     }
 
     static setDeviceLocation(latitude, longitude) {
-        if (latitude == null ||
-            longitude == null) {
-            console.log('UserExperior : setDeviceLocation params are invalid');
-            return;
+        if (Platform.OS === 'android') {
+            if (latitude == null ||
+                longitude == null) {
+                console.log('UserExperior : setDeviceLocation params are invalid');
+                return;
+            }
+            UserExperiorBridge.setDeviceLocation(latitude, longitude);
+        
         }
-        UserExperiorBridge.setDeviceLocation(latitude, longitude);
-    }*/
+    }
 	
 	static addInSecureViewBucket(ref) {
-        if (ref == null) {
-            console.log('UserExperior : Can\'t add a null ref to secure view bucket (are you sure the component has a ref?)');
-            return;
+        if (Platform.OS === 'android') {
+            if (ref == null) {
+                console.log('UserExperior : Can\'t add a null ref to secure view bucket (are you sure the component has a ref?)');
+                return;
+            }
+    
+            UserExperiorBridge.addInSecureViewBucket(findNodeHandle(ref));
+            console.log('UserExperior: masking done!!!!!!!!!!!!!!!!!!!!!!!!');
         }
-
-        UserExperiorBridge.addInSecureViewBucket(findNodeHandle(ref));
-		console.log('UserExperior: masking done!!!!!!!!!!!!!!!!!!!!!!!!');
     }
 
     static removeFromSecureViewBucket(ref) {
-        if (ref == null) {
-            console.log('UserExperior : Can\'t remove a null ref from secure view bucket (are you sure the component has a ref?)');
-            return;
+        if (Platform.OS === 'android') {
+            if (ref == null) {
+                console.log('UserExperior : Can\'t remove a null ref from secure view bucket (are you sure the component has a ref?)');
+                return;
+            }
+            
+            UserExperiorBridge.removeFromSecureViewBucket(findNodeHandle(ref));
         }
-        
-        UserExperiorBridge.removeFromSecureViewBucket(findNodeHandle(ref));
     }
 
     static optIn() {
-        UserExperiorBridge.optIn();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.optIn();
+        }  
     }
 
     static optOut() {
-        UserExperiorBridge.optOut();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.optOut();
+        }  
     }
 
     static getOptOutStatus() {
-        UserExperiorBridge.getOptOutStatus();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.getOptOutStatus();
+        }  
     }
 
     static consent() {
-        UserExperiorBridge.consent();
+        if (Platform.OS === 'android') {
+            UserExperiorBridge.consent();
+        }  
     }
 }
 
