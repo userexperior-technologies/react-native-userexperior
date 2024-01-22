cacbed3a48233d42dc353444c1e1539ad5de2f53
Zeeshan Ali Sayyed <zeeshan@userexperior.com>
2024-01-22 14:48:20 +0530

    updated ue android sdk version to 7.3.0 in build.gradle: which has fix to sdk not working when app is updated due to older encryption algo key, iv and new sdk code with new algo; updated ue react native plugin version to 3.0.6 in package.json and .js files;

--- react-native-userexperior/UserExperior.js
+++ react-native-userexperior/UserExperior.js
@@ -3,7 +3,7 @@ var { NativeModules, findNodeHandle, InteractionManager } = require('react-nativ
 var UserExperiorBridge = NativeModules.UserExperior;
 
 const fw = "rn"; // framework: React-Native
-const sv = "3.0.5"; // SDK/Plugin Version
+const sv = "3.0.6"; // SDK/Plugin Version
 
 class UserExperior {
 
